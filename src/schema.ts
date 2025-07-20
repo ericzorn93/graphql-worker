243a944764d7efd6cefd6647d17210e136982687
Eric Zorn <zornwebdev@gmail.com>
2025-07-20 15:19:28 -0400

    feat: adding field resolvers

--- src/schema.ts
+++ src/schema.ts
@@ -1,4 +1,4 @@
-import { ObjectType, Field, Resolver, Query, ID, buildSchema, Int, Ctx } from 'type-graphql';
+import { ObjectType, Field, Resolver, Query, ID, buildSchema, Int, Ctx, FieldResolver } from 'type-graphql';
 import Context from './context';
 import Container, { Service } from 'typedi';
 
@@ -6,9 +6,6 @@ import Container, { Service } from 'typedi';
 class User {
 	@Field(() => ID)
 	id!: string;
-
-	@Field(() => String, { description: 'Name of the user' })
-	name: string;
 
 	@Field(() => Address, { nullable: true })
 	address?: Address;
@@ -31,9 +28,14 @@ class Address {
 
 @Service()
 class UserService {
-	public getName(): string {
-		console.log('Fetching user name...');
-		return 'Eric Zorn';
+	public getFirstName(): string {
+		console.log('Fetching first name...');
+		return 'Eric';
+	}
+
+	public getLastName(): string {
+		console.log('Fetching last name...');
+		return 'Zorn';
 	}
 }
 
@@ -47,7 +49,6 @@ class UserResolver {
 		console.log({ name: ctx.appName, ipAddress: ctx.ipAddress }); // Log the IP address for debugging
 		return {
 			id: '1',
-			name: this.userService.getName(),
 			address: {
 				street: '123 Main St',
 				city: 'Anytown',
@@ -55,6 +56,21 @@ class UserResolver {
 				zip: 12345,
 			},
 		};
+	}
+
+	@FieldResolver(() => String, { description: 'First name of the user' })
+	public firstName(): string {
+		return this.userService.getFirstName();
+	}
+
+	@FieldResolver(() => String, { description: 'Last name of the user' })
+	public lastName(): string {
+		return this.userService.getLastName();
+	}
+
+	@FieldResolver(() => String, { description: 'Full name of the user' })
+	public fullName(): string {
+		return `${this.userService.getFirstName()} ${this.userService.getLastName()}`;
 	}
 }
 
