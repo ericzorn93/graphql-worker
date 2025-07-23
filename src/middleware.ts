9278720a047b74765ac93f24e4fab02c290b59fe
Eric Zorn <zornwebdev@gmail.com>
2025-07-23 07:31:01 -0400

    feat: adding middleware

---
+++ src/middleware.ts
@@ -0,0 +1,17 @@
+import { MiddlewareInterface, ResolverData, NextFn } from 'type-graphql';
+import { Inject, Service } from 'typedi';
+
+import Context from './context';
+import { EnvToken } from './di_tokens';
+
+@Service()
+export class WriteLastAcesses implements MiddlewareInterface<Context> {
+	constructor(@Inject(EnvToken) private readonly env: Env) {}
+
+	async use(_resolverData: ResolverData<Context>, next: NextFn) {
+		const now = new Date();
+		await this.env.GRAPHQL_WORKER_KV.put('lastAccessedAt', now.toISOString());
+		console.info(`Last accessed at: ${now.toISOString()}`);
+		return next();
+	}
+}
